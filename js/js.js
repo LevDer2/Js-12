6f2b11c600e3c7950099709704f3a079b14cef21
LevDer2 <123574272+LevDer2@users.noreply.github.com>
2025-04-27 16:44:34 +0300

    2

--- js/js.js
+++ js/js.js
@@ -5,21 +5,77 @@
 // замінює значення premium на false
 // виводить вміст об'єкта user в форматі ключ:значення використовуючи Object.keys() і for...of
 
-const user = {
-    name: "Lev",
-    age: 13,
-    hobby: "programming",
-    premium: true,
-}
+// const user = {
+//     name: "Lev",
+//     age: 13,
+//     hobby: "programming",
+//     premium: true,
+// }
 
-user.mood = "Normal";
-user.hobby = 'skydiving';
-user.premium = false;
-const keys = Object.keys(user);
-console.log(keys);
-console.log(user);
+// user.mood = "Normal";
+// user.hobby = 'skydiving';
+// user.premium = false;
+// const keys = Object.keys(user);
+// console.log(keys);
+// console.log(user);
 
-for (let i of keys) {
-console.log(`${i}: ${user[i]}`);
+// for (let i of keys) {
+// console.log(`${i}: ${user[i]}`);
 
-}
+// }
+
+// Завдання 2
+// Напиши функцію countProps(obj), яка рахує кількість властивостей в об'єкті. Функція повертає число — кількість властивостей.
+
+// const user = {
+//     name: "Lev",
+//     age: 13,
+//     hobby: "programming"
+//   };
+  
+//   const countProps = function(obj) {
+//     const keyy = Object.keys(obj);
+//     return console.log(`${keyy.length}`);
+// };
+//   countProps(user);
+
+//   Завдання 3
+// Напиши функцію findBestEmployee(employees), яка приймає об'єкт співробітників і повертає ім'я найпродуктивнішого (який виконав більше всіх задач). Співробітники і кількість виконаних завдань містяться як властивості об'єкта в форматі "ім'я":"кількість задач".
+
+const employeers = {
+    brobDev: {
+        name: "BoBA",
+        tasks: 5,
+    },
+    justDev: {
+        name: "Some",
+        tasks: 10,
+    },
+    lazyDev: {
+        name: "naourami",
+        tasks: 4,
+    },
+    freelancerForLife: {
+        name: "Lev",
+        tasks: 15,
+    },
+};
+const findBestEmployee = function(employees){
+    const keys = Object.keys(employees);
+    let maxTasks = 0;
+    let bestEmployee = "";
+
+    for (let key of keys){
+        const worker = employees[key];  
+        const tasks = worker.tasks;
+        
+        if (tasks > maxTasks){
+            maxTasks = tasks;
+            bestEmployee = worker.name;
+        }
+    }
+
+    return  console.log(`Найкращий працівник ${bestEmployee}! Тому , що він виконав ${maxTasks} тасків`);
+    
+};
+console.log(findBestEmployee(employeers));
