69a09f91dc4c87f9b121ff64763d5a2f7516de6d
LevDer2 <123574272+LevDer2@users.noreply.github.com>
2025-04-30 10:35:32 +0300

    end

--- js/js.js
+++ js/js.js
@@ -42,43 +42,31 @@
 //   Завдання 3
 // Напиши функцію findBestEmployee(employees), яка приймає об'єкт співробітників і повертає ім'я найпродуктивнішого (який виконав більше всіх задач). Співробітники і кількість виконаних завдань містяться як властивості об'єкта в форматі "ім'я":"кількість задач".
 
-// const employeers = {
-//     brobDev: {
-//         name: "BoBA",
-//         tasks: 5,
-//     },
-//     justDev: {
-//         name: "Some",
-//         tasks: 10,
-//     },
-//     lazyDev: {
-//         name: "naourami",
-//         tasks: 4,
-//     },
-//     freelancerForLife: {
-//         name: "Lev",
-//         tasks: 15,
-//     },
-// };
-// const findBestEmployee = function(employees){
-//     const keys = Object.keys(employees);
-//     let maxTasks = 0;
-//     let bestEmployee = "";
+// const work = {
+//   vlad: 20,
+//   mukola: 50,
+//   lev: 100,
+//   nazar: 110,
+//   vlad2: 54,
+//   david: 70,
+// }
 
-//     for (let key of keys){
-//         const worker = employees[key];  
-//         const tasks = worker.tasks;
-        
-//         if (tasks > maxTasks){
-//             maxTasks = tasks;
-//             bestEmployee = worker.name;
-//         }
-//     }
+// const findBestEmployee = function (employees) {
+//   let mostValue = '';
+//   let maxValue = 0;
+//   for (const employ in employees) {
+//       // console.log(employ); // ім'я
+//       // console.log(employees[employ]); // число
+//       if (employees[employ] > maxValue) {
+//           maxValue = employees[employ]
+//           mostValue = employ;
+//       }
+//   }
+//   return `${mostValue}: ${maxValue}`
+// }
 
-//     return  console.log(`Найкращий працівник ${bestEmployee}! Тому , що він виконав ${maxTasks} тасків`);
-    
-// };
-// findBestEmployee(employeers);
+// console.log(findBestEmployee(work));
+
 
 // Завдання 4
 // Напиши функцію countTotalSalary(employees) приймаючу об'єкт зарплат. Функція рахує загальну суму зарплати працівників і повертає її. Кожне поле об'єкта, переданого в функцію, має вигляд "ім'я":"зарплата".
@@ -106,14 +94,80 @@
 //   Завдання 5
 // Напиши функцію getAllPropValues(arr, prop), яка отримує масив об'єктів і ім'я властивості. Повертає масив значень певної властивості prop з кожного об'єкта в масиві.
 
+// const getAllPropValues = function (arr, prop) {
+//     // console.log(arr);
+//     // console.log(prop);
+//     let result = []
+//     for (const item of arr) {
+//       // console.log(item);
+//       // console.log(item[prop]);
+//       result.push(item[prop])
+//     }
+//     return result
+// };
+
+//   const products = [
+//     { 
+//       name: 'Радар', 
+//       price: 1300, 
+//       quantity: 4 },
+//     { 
+//       name: 'Сканер',
+//        price: 2700, 
+//        quantity: 3 },
+//     { 
+//       name: 'Дроїд', 
+//       price: 400, 
+//       quantity: 7 },
+//     { 
+//       name: 'Захоплення', 
+//       price: 1200, 
+//       quantity: 2 },
+//   ];
+  
+  
+
+
+// console.log(getAllPropValues(products, "name"));
+// console.log(getAllPropValues(products, "price"));
+// console.log(getAllPropValues(products, "quantity"));
+
+// Завдання 6
+// Напиши функцію calculateTotalPrice(allProdcuts, productName), яка отримує масив об'єктів та ім'я продукту (значення властивості name). Повертає загальну вартість продукту (ціна * кількість).
+// Викличи функції для перевірки працездатності твоєї реалізації.
+
+const calculateTotalPrice = function (allProdcuts, productName) {
+  // console.log(allProdcuts);
+  // console.log(productName);
+  for (const product of allProdcuts)  {
+    // console.log(product);
+    if (product.name === productName) {
+      const result = product.price * product.quantity
+      return `Загальна сума ${product.name} = ${result}`
+    }
+  }
+};
+
   const products = [
-    { name: 'Радар', price: 1300, quantity: 4 },
-    { name: 'Сканер', price: 2700, quantity: 3 },
-    { name: 'Дроїд', price: 400, quantity: 7 },
-    { name: 'Захоплення', price: 1200, quantity: 2 },
+    { 
+      name: 'Радар', 
+      price: 1300, 
+      quantity: 4 },
+    { 
+      name: 'Сканер',
+       price: 2700, 
+       quantity: 3 },
+    { 
+      name: 'Дроїд', 
+      price: 400, 
+      quantity: 7 },
+    { 
+      name: 'Захоплення', 
+      price: 1200, 
+      quantity: 2 },
   ];
-  
-  
-  const getAllPropValues = function (arr, prop) {
-    
-  };
+
+console.log(calculateTotalPrice(products, "Сканер"));
+console.log(calculateTotalPrice(products, "Радар"));
+console.log(calculateTotalPrice(products, "Дроїд"));
+console.log(calculateTotalPrice(products, "Захоплення"));
