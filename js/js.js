322adea2bd0a211d3d13468748ce960b405bc909
LevDer2 <123574272+LevDer2@users.noreply.github.com>
2025-05-04 12:51:37 +0300

    js

--- js/js.js
+++ js/js.js
@@ -1,9 +1,9 @@
-// Завдання 1
-// Напиши скрипт, який, для об'єкта user, послідовно:
-// додає поле mood зі значенням 'happy'
-// замінює значення hobby на 'skydiving'
-// замінює значення premium на false
-// виводить вміст об'єкта user в форматі ключ:значення використовуючи Object.keys() і for...of
+// // Завдання 1
+// // Напиши скрипт, який, для об'єкта user, послідовно:
+// // додає поле mood зі значенням 'happy'
+// // замінює значення hobby на 'skydiving'
+// // замінює значення premium на false
+// // виводить вміст об'єкта user в форматі ключ:значення використовуючи Object.keys() і for...of
 
 // const user = {
 //     name: "Lev",
@@ -12,35 +12,43 @@
 //     premium: true,
 // }
 
+// const { names = "Nazar", ages = 17, hobbys = "programming", premium = false, ...rest}  = user;
+// // console.log(name, age, hobby, premium);
+
+
 // user.mood = "Normal";
 // user.hobby = 'skydiving';
 // user.premium = false;
 // const keys = Object.keys(user);
 // console.log(keys);
 // console.log(user);
-
+// console.log(names, ages, hobbys, premium, rest);
 // for (let i of keys) {
 // console.log(`${i}: ${user[i]}`);
 
 // }
 
-// Завдання 2
-// Напиши функцію countProps(obj), яка рахує кількість властивостей в об'єкті. Функція повертає число — кількість властивостей.
+// // Завдання 2
+// // Напиши функцію countProps(obj), яка рахує кількість властивостей в об'єкті. Функція повертає число — кількість властивостей.
 
-// const user = {
+// const users = {
 //     name: "Lev",
 //     age: 13,
 //     hobby: "programming"
 //   };
+// const { name = "Nazar", age = 17, hobby = "programming", }  = users;
+//   console.log(name,age,hobby);
+  
   
 //   const countProps = function(obj) {
 //     const keyy = Object.keys(obj);
 //     return console.log(`${keyy.length}`);
 // };
-//   countProps(user);
+//   countProps(users);
+//   countProps(name,age,hobby);
 
-//   Завдання 3
-// Напиши функцію findBestEmployee(employees), яка приймає об'єкт співробітників і повертає ім'я найпродуктивнішого (який виконав більше всіх задач). Співробітники і кількість виконаних завдань містяться як властивості об'єкта в форматі "ім'я":"кількість задач".
+// //   Завдання 3
+// // Напиши функцію findBestEmployee(employees), яка приймає об'єкт співробітників і повертає ім'я найпродуктивнішого (який виконав більше всіх задач). Співробітники і кількість виконаних завдань містяться як властивості об'єкта в форматі "ім'я":"кількість задач".
 
 // const work = {
 //   vlad: 20,
@@ -50,6 +58,12 @@
 //   vlad2: 54,
 //   david: 70,
 // }
+
+// const {vlad, mukola, lev, nazar, vlad2, david} = work;
+// console.log(work);
+
+// console.log(vlad, mukola, lev, nazar, vlad2, david);
+
 
 // const findBestEmployee = function (employees) {
 //   let mostValue = '';
@@ -67,32 +81,36 @@
 
 // console.log(findBestEmployee(work));
 
+// // Завдання 4
+// // Напиши функцію countTotalSalary(employees) приймаючу об'єкт зарплат. Функція рахує загальну суму зарплати працівників і повертає її. Кожне поле об'єкта, переданого в функцію, має вигляд "ім'я":"зарплата".
 
-// Завдання 4
-// Напиши функцію countTotalSalary(employees) приймаючу об'єкт зарплат. Функція рахує загальну суму зарплати працівників і повертає її. Кожне поле об'єкта, переданого в функцію, має вигляд "ім'я":"зарплата".
+const fruit ={
+  mango: 100,
+  poly: 150,
+  alfred: 80,
+}
 
-// const countTotalSalary = function(employees) {
-//     let totalSalary = 0;
-//     const values = Object.values(employees);
+const {mango, poly, alfred,} = fruit;
+console.log(mango, poly, alfred);
+
+
+const countTotalSalary = function(employees) {
+    let totalSalary = 0;
+    const values = Object.values(employees);
+    for (let i of values) {
+      totalSalary += i;
+      console.log(totalSalary);
+    }
     
-//     for (let i of values) {
-//       totalSalary += i;
-//       console.log(totalSalary);
-//     }
-    
-//     return totalSalary;
-//   }
+    return totalSalary;
+  }
   
-//   console.log(
-//     countTotalSalary({
-//       mango: 100,
-//       poly: 150,
-//       alfred: 80,
-//     }),
-//   ); // 330
+  console.log(countTotalSalary(fruit)); // 330
+  console.log(countTotalSalarys(mango, poly, alfred));
+  
 
-//   Завдання 5
-// Напиши функцію getAllPropValues(arr, prop), яка отримує масив об'єктів і ім'я властивості. Повертає масив значень певної властивості prop з кожного об'єкта в масиві.
+// //   Завдання 5
+// // Напиши функцію getAllPropValues(arr, prop), яка отримує масив об'єктів і ім'я властивості. Повертає масив значень певної властивості prop з кожного об'єкта в масиві.
 
 // const getAllPropValues = function (arr, prop) {
 //     // console.log(arr);
@@ -106,7 +124,7 @@
 //     return result
 // };
 
-//   const products = [
+//   const productss = [
 //     { 
 //       name: 'Радар', 
 //       price: 1300, 
@@ -128,46 +146,46 @@
   
 
 
-// console.log(getAllPropValues(products, "name"));
-// console.log(getAllPropValues(products, "price"));
-// console.log(getAllPropValues(products, "quantity"));
+// console.log(getAllPropValues(productss, "name"));
+// console.log(getAllPropValues(productss, "price"));
+// console.log(getAllPropValues(productss, "quantity"));
 
-// Завдання 6
-// Напиши функцію calculateTotalPrice(allProdcuts, productName), яка отримує масив об'єктів та ім'я продукту (значення властивості name). Повертає загальну вартість продукту (ціна * кількість).
-// Викличи функції для перевірки працездатності твоєї реалізації.
+// // Завдання 6
+// // Напиши функцію calculateTotalPrice(allProdcuts, productName), яка отримує масив об'єктів та ім'я продукту (значення властивості name). Повертає загальну вартість продукту (ціна * кількість).
+// // Викличи функції для перевірки працездатності твоєї реалізації.
 
-const calculateTotalPrice = function (allProdcuts, productName) {
-  // console.log(allProdcuts);
-  // console.log(productName);
-  for (const product of allProdcuts)  {
-    // console.log(product);
-    if (product.name === productName) {
-      const result = product.price * product.quantity
-      return `Загальна сума ${product.name} = ${result}`
-    }
-  }
-};
+// const calculateTotalPrice = function (allProdcuts, productName) {
+//   // console.log(allProdcuts);
+//   // console.log(productName);
+//   for (const product of allProdcuts)  {
+//     // console.log(product);
+//     if (product.name === productName) {
+//       const result = product.price * product.quantity
+//       return `Загальна сума ${product.name} = ${result}`
+//     }
+//   }
+// };
 
-  const products = [
-    { 
-      name: 'Радар', 
-      price: 1300, 
-      quantity: 4 },
-    { 
-      name: 'Сканер',
-       price: 2700, 
-       quantity: 3 },
-    { 
-      name: 'Дроїд', 
-      price: 400, 
-      quantity: 7 },
-    { 
-      name: 'Захоплення', 
-      price: 1200, 
-      quantity: 2 },
-  ];
+//   const products = [
+//     { 
+//       name: 'Радар', 
+//       price: 1300, 
+//       quantity: 4 },
+//     { 
+//       name: 'Сканер',
+//        price: 2700, 
+//        quantity: 3 },
+//     { 
+//       name: 'Дроїд', 
+//       price: 400, 
+//       quantity: 7 },
+//     { 
+//       name: 'Захоплення', 
+//       price: 1200, 
+//       quantity: 2 },
+//   ];
 
-console.log(calculateTotalPrice(products, "Сканер"));
-console.log(calculateTotalPrice(products, "Радар"));
-console.log(calculateTotalPrice(products, "Дроїд"));
-console.log(calculateTotalPrice(products, "Захоплення"));
+// console.log(calculateTotalPrice(products, "Сканер"));
+// console.log(calculateTotalPrice(products, "Радар"));
+// console.log(calculateTotalPrice(products, "Дроїд"));
+// console.log(calculateTotalPrice(products, "Захоплення"));
